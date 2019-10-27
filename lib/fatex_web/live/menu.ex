d2c503028491ddb8b0c8c25cc2156ee385ee482e
vinicius molina <vinicius_molina99@hotmail.com>
2019-10-27 16:32:26 -0300

    add button menu for each step

--- lib/fatex_web/live/menu.ex
+++ lib/fatex_web/live/menu.ex
@@ -6,9 +6,16 @@ defmodule FatexWeb.MenuLive do
     FatexWeb.MenuView.render("index.html", assigns)
   end
 
+  def mount(%{step_id: nil} = _session, socket) do
+    step = LatexConfigs.get_step(1)
+    templates =LatexConfigs.list_template_sections_from_step(step)
+
+    {:ok, assign(socket, templates: templates, to_add: nil)}
+  end
+
   def mount(%{step_id: id} = _session, socket) do
     step = LatexConfigs.get_step(id)
-    templates = LatexConfigs.list_template_sections_from_step(step)
+    templates =LatexConfigs.list_template_sections_from_step(step)
 
     {:ok, assign(socket, templates: templates, to_add: nil)}
   end
--- lib/fatex_web/live/step.ex
+++ lib/fatex_web/live/step.ex
@@ -6,8 +6,16 @@ defmodule FatexWeb.StepLive do
     FatexWeb.StepView.render("index.html", assigns)
   end
 
+  def mount(%{step_id: nil} = _session, socket) do
+    root_sections =
+      LatexConfigs.get_step(1)
+      |> LatexConfigs.list_sections_from_step()
+
+    {:ok, assign(socket, root_sections: root_sections)}
+  end
+
   def mount(%{step_id: id} = _session, socket) do
-    root_sections = 
+    root_sections =
       LatexConfigs.get_step(id)
       |> LatexConfigs.list_sections_from_step()
 
