dc77ccd066983f3808b2900d66d6a7f74d8f2b24
vinicius molina <vinicius_molina99@hotmail.com>
2019-12-08 00:07:54 -0300

    add docker and images for template

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :fatex, Fatex.Repo,
   username: "postgres",
   password: "postgres",
   database: "fatex_dev",
-  hostname: "localhost",
+  hostname: "0.0.0.0",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
